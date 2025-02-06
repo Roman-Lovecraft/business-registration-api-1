46a637f794ace33fbcbc23cadd18cb78cc248005
Roman-Lovecraft <roman.lovecraft@approol.com>
2025-02-06 05:35:34 +0300

    version 67898765

--- app/main.py
+++ app/main.py
@@ -10,17 +10,14 @@ async def register_business(response: RequestResponse):
     Принимает JSON с данными регистрации, валидирует их и заполняет форму на сайте.
     Возвращает {"status": "success", "response": "...", "state": "..."} или сообщение об ошибке.
     """
-    try:
-        # Запуск функции для выбранного штата
-        response_data = selenium_handlers.fill_form(response.state, response.credentials, response.data)
-        
-        # Ответ в формате JSON
-        return {"status": "success", "response": response_data, "state": response.state.upper()}
 
-    except ValueError as ve:
-        raise HTTPException(status_code=400, detail={"status": "error", "message": str(ve), "state": response.state.upper()})
-    except Exception as e:
-        raise HTTPException(status_code=500, detail={"status": "error", "message": str(e), "state": response.state.upper()})
+    # Запуск функции для выбранного штата
+    response_data = selenium_handlers.fill_form(response.state, response.credentials, response.data)
+        
+    # Ответ в формате JSON
+    return {"status": "success", "response": response_data, "state": response.state.upper()}
+
+
 
 if __name__ == "__main__":
     import uvicorn
